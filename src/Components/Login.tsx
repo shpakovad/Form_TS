11cf37df9f0c894dce50a711d2b7d1467fd48a77
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-01-17 19:58:48 +0300

    create branch login Page

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,16 +1,30 @@
-
 import React from 'react';
 
 interface IProps {
 
-}
+};
 
-const Login=(props:IProps)=>{
-return (
-    <div>
-        Login Form
-    </div>
-)
+
+const Login = (props: IProps) => {
+    return (
+        <div>
+            <h1> SignIn </h1>
+            <form>
+                <span> e-mail </span> <input />
+                <div>
+                    <span> password </span> <input type='password'/>
+                </div>
+                <a href='#'> Forgot password</a>
+                <div>
+                    <input type='checkbox'/> <span>Remember me</span>
+                </div>
+                <button type='submit'>Sign in</button>
+                <div>
+                    <a href='#'> Registration</a>
+                </div>
+            </form>
+        </div>
+    )
 
 };
 
