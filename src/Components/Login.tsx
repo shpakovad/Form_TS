5da1a6afe912d3699ae4f75a3ba44662005a134e
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-01-17 20:21:18 +0300

    add events

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 interface IProps {
 
 };
 
-
 const Login = (props: IProps) => {
+
+    const [email, setEmail] = useState('');
+    let onChangeEmail=((e:ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    });
+
+    const [password, setPassword] = useState('');
+    let onChangePassword=((e:ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    });
+
     return (
         <div>
             <h1> SignIn </h1>
             <form>
-                <span> e-mail </span> <input />
+                <span> e-mail </span> <input onChange={onChangeEmail} value={email} />
                 <div>
-                    <span> password </span> <input type='password'/>
+                    <span> password </span> <input type='password' onChange={onChangePassword} value={password} />
                 </div>
-                <a href='#'> Forgot password</a>
+                <a href='/forgot'> Forgot password</a>
                 <div>
                     <input type='checkbox'/> <span>Remember me</span>
                 </div>
