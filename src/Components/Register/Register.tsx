54c001bae0ee9459fd6d903e0f50fc544c6ba90f
Yury <goldyukol12@gmail.com>
2020-01-17 19:58:00 +0300

    feat: add local state and change register form

--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import s from './Register.module.css'
 
 interface IProps {
@@ -6,11 +6,23 @@ interface IProps {
 }
 
 const Register = (props: IProps) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState();
+
+    const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+
+    const onChangePass = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    }
+
     return (<div className={s.wrapper}>
         <div>Register</div>
-        <input value="test@email.nya"></input>
-        <input type="password" value="test password nya"></input>
-        <input type="password" value="test password nya2"></input>
+        <input value={email} onChange={onChangeEmail}></input>
+        <input type='password' value={password}></input>
+        <input type='password' value={password}></input>
         <button>Register</button>
         <a href="#/sign-in">Sign In</a>
     </div>)
